1ade6ffadff18e98c6eed8d5e5be1ca6986af7b9
knaito <knaito@asial.co.jp>
2018-09-11 18:37:58 +0900

    npm install from local directory

--- integration-tests/first.spec.js
+++ integration-tests/first.spec.js
@@ -1,6 +1,9 @@
 var child_process = require('child_process');
 var which = require('which');
 var helpers = require('./helpers');
+var path = require('path');
+var fs = require('fs-extra');
+var iswin32 = process.platform === 'win32';
 
 const TIMEOUT = 60 * 1000;
 
@@ -9,6 +12,10 @@ describe('first tests', function () {
     const testProject = helpers.tmpDir('first_test_project');
     beforeAll(function() {
         process.chdir(testProject);
+    });
+
+    afterAll(function() {
+
     });
 
     it('Test 001 : npm install begin', function () {
@@ -21,13 +28,50 @@ describe('first tests', function () {
             });
     });
 
-    it('Test 002 : npm install from local directory', function () {
+    it('Test 002 : spawn child_process', function () {
         return Promise.resolve()
             .then(function () {
                 return new Promise(function (resolve, reject) {
                     var path_npm = which.sync('npm');
                     console.log(path_npm);
                     var cp = child_process.spawn(path_npm, ['-v']);
+                    cp.on('exit', function (code) {
+                        expect(1).toBe(1);
+                        resolve();
+                    });
+                    cp.stdout.setEncoding('utf-8');
+                    cp.stdout.on('data', function (data) {
+                        console.log(data);
+                    });
+                });
+            });
+    });
+
+    it('Test 003 : npm install from local directory', function () {
+        return Promise.resolve()
+            .then(function () {
+                return new Promise(function (resolve, reject) {
+                    var modpath = path.resolve(__dirname, 'fixtures', 'modules', 'test01');
+                    console.log("modpath:" + modpath);
+                    var path_npm = which.sync('npm');
+                    var cp = child_process.spawn(path_npm, ['install', modpath]);
+                    cp.on('exit', function (code) {
+                        expect(1).toBe(1);
+                        resolve();
+                    });
+                    cp.stdout.setEncoding('utf-8');
+                    cp.stdout.on('data', function (data) {
+                        console.log(data);
+                    });
+                });
+            }).then(function () {
+                return new Promise(function (resolve, reject) {
+                    var cp = null;
+                    if (iswin32) {
+                        cp = child_process.spawn('dir', []);
+                    } else {
+                        cp = child_process.spawn('ls', ['-alh']);
+                    }
                     cp.on('exit', function (code) {
                         expect(1).toBe(1);
                         resolve();
